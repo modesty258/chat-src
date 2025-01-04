1fc994399db95ed0cc315f69b4cc9585c796ea4b
Chenghao Liu <129705482+modesty258@users.noreply.github.com>
2025-01-04 20:08:21 +0800

    Add files via upload
    客户端

---
+++ student_client/src/views/Admin/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-container>
+      <el-main>
+        <h1>admin home</h1>
+        <el-card style="width: 40%">
+          <p><i class="el-icon-s-home" style="margin-right: 18px"></i>项目名：学生管理系统</p>
+          <p><i class="el-icon-location-outline" style="margin-right: 18px"></i>山东科技大学</p>
+
+        </el-card>
+        <logout></logout>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Logout from "@/components/logout";
+export default {
+  name: "home",
+  components: {Logout},
+}
+</script>
+
+<style scoped>
+
+</style>
